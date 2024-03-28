90ec2e5fd0e017a40b7b21d5cd0954556268e7bd
dimassdoubles <codedimas@gmail.com>
2024-03-28 19:45:46 +0700

    feat: tailwind variant

--- components/theme-toggler.tsx
+++ components/theme-toggler.tsx
@@ -1,16 +1,27 @@
 "use client"
 
+import { variant } from "@/lib/utils"
 import { Button } from "./ui/button"
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
+
+const toggleStyle = variant(
+    "h-[1.2rem] w-[1.2rem] transition-all",
+    {
+        type: {
+            light: "rotate-0 scale-100 dark:rotate-90 dark:scale-0",
+            dark: "absolute rotate-90 scale-0 dark:rotate-0 dark:scale-100",
+        }
+    }
+)
 
 const ThemeToggler = () => {
     const { theme, setTheme } = useTheme()
 
     return (
         <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0"/>
-            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+            <SunIcon className={toggleStyle({type: "light"})}/>
+            <MoonIcon className={toggleStyle({type: "dark"})}/>
         </Button>
     )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+export function variant(base: string, variants: {[key: string]: any}) {
+   return function builder(props: {[key: string]: any}) {
+    const classes: string[] = []
+    Object.entries(props).forEach(([key, value]) => {
+      if (!(key in variants)) {
+        return console.warn("not found", key)   
+      }
+      classes.push(variants[key][value])
+    })
+
+    return cn(base, ...classes)
+   }
+}
